84f8e16ed251dd5a75d5186322681504f7c83b80
Muhammad Willdhan Arya Putra <64698182+0x256e4@users.noreply.github.com>
2021-08-31 09:18:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import time
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
 
 options = Options()
-options.add_argument("--headless")
+options2 = Options()
+#options.add_argument("--headless")
+options2.add_argument("--headless")
 driver = Firefox(options=options)
+driver2 = Firefox(options=options2)
 
 class Email:
     def getEmail():
@@ -28,7 +32,35 @@ class Email:
         email = emailId.get_attribute("innerHTML")
         return email
 
+    def refreshEmail():
+        WebDriverWait(driver, 300).until(lambda button: button.find_element_by_xpath("/html/body/div[4]/div/div/div/div[2]/div[2]/div[4]/div[3]/center/div/table/tbody/tr/td/table[2]/tbody/tr/td/table[2]/tbody/tr[4]/td/table/tbody/tr/th[1]/table/tbody/tr/td/a"))
+        driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[2]/div[2]/div[4]/div[3]/center/div/table/tbody/tr/td/table[2]/tbody/tr/td/table[2]/tbody/tr[4]/td/table/tbody/tr/th[1]/table/tbody/tr/td/a").click()
+
+class QwiklabsAccount:
+    def createAccount():
+        #work in progress feature
+        pass
+
+    def createBonus():
+        email = Email.getEmailName()
+        driver2.get("https://inthecloud.withgoogle.com/google-cloud-skills/register.html")
+        driver2.find_element_by_xpath("//*[@id='FirstName']").send_keys("tulang")
+        driver2.find_element_by_xpath("//*[@id='LastName']").send_keys("ikan")
+        driver2.find_element_by_xpath("//*[@id='Company']").send_keys("{}".format(email))
+        driver2.find_element_by_xpath("//*[@id='Email']").send_keys("{}".format(email))
+        driver2.find_element_by_xpath("/html/body/main/div[2]/div[1]/section[20]/div/div/form/div[6]/div/div/select/option[7]").click()
+        driver2.find_element_by_xpath("//*[@id='Phone']").send_keys("1337")
+        driver2.find_element_by_xpath("/html/body/main/div[2]/div[1]/section[20]/div/div/form/div[8]/div/div/select/option[107]").click()
+        driver2.find_element_by_xpath("/html/body/main/div[2]/div[1]/section[20]/div/div/form/div[11]/div/div/select/option[2]").click()
+        driver2.find_element_by_xpath("//*[@id='mktoRadio_959319_0']").click()
+        driver2.find_element_by_xpath("//*[@id='mktoRadio_959305_0']").click()
+        driver2.find_element_by_xpath("/html/body/main/div[2]/div[1]/section[20]/div/div/form/div[27]/span/button").click()
+
+
 
 if __name__ == "__main__":
     Email.getEmail()
     print("Your current email account : {}".format(Email.getEmailName()))
+    QwiklabsAccount.createBonus()
+    print("Appeal is proceed")
+    Email.refreshEmail()
